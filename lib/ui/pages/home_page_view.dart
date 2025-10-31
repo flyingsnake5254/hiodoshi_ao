85ac015a4e2b2025d7510dc8dd9b9681c8b3213e
sherloxk <sherloxk5254@gmail.com>
2025-10-31 11:11:52 +0800

    update 大岩壁

--- lib/ui/pages/home_page_view.dart
+++ lib/ui/pages/home_page_view.dart
@@ -12,7 +12,7 @@ class HomePageView extends StatelessWidget {
     return BaseView(
         builder: (context, model, child) {
           return Scaffold(
-
+            body: Text("大岩壁"),
           );
         },
         modelProvider: () => HomePageViewModel(),
